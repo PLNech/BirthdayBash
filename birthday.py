300ea9b5f15d36eae10f545fb5f65c73068bac4b
Paul Louis NECH <paul-louis@swiftkey.com>
2014-09-16 17:34:50 +0100

    Now fetching from GDocs

--- birthday.py
+++ birthday.py
@@ -2,30 +2,48 @@
 #birthday.py
 #A script to tell you who's birthday it is
 #Author: paul-louis
-#TODO: Handle real csv and not csv+comapunkt
-#TODO: Get the last data from the internet
+#TODO: Parameter the user/pass
 #TODO: Wish the lucky guy a wonderful birthday!
 #
+
+from time import strftime
+import xlrd, csv
+import os, sys
+import gspread
+import getpass
+
+def xlsxToCSV(filename, sheetname):
+    wb = xlrd.open_workbook(filename)
+    sh = wb.sheet_by_name(sheetname)
+    out = open(filename.replace('xlsx', 'csv'), 'wb')
+    wr = csv.writer(out, quoting=csv.QUOTE_ALL)
+    iterator = 0
+    while iterator < sh.nrows:
+        line = sh.row_values(iterator)
+        line = [str(w).replace(',', ';') for w in line]
+        wr.writerow(line)
+        iterator += 1;
+    out.close()
 
 def getBirthdays(day, month):
     months = []
     dayCounter = 0
-    import csv
-    with open('Birthdays.csv', 'rb') as csvfile:
+    with open('Birth.csv', 'rb') as csvfile:
         linereader = csv.reader(csvfile, delimiter='\n', quotechar='|')
         for row in linereader:
             monthCounter = 1
             dayCounter +=1
             if dayCounter == 1:
                 for name in row[0].split(','):
-                    months.append(name)
+                    months.append(name.strip('"'))
                 continue
             if dayCounter > 32:
-                    break
+                break
 
             guys = ', '.join(row).split(',')
             guys.pop(0)
             for name in guys:
+                name = name.strip('"')
                 if (name == ''):
                     name = 'Nobody'
                 monthCounter += 1
@@ -38,13 +56,36 @@ def getBirthdays(day, month):
 
     return months
 
-
-def birthday():
-    print "Happy Birthday"
+def fetchSpreadsheet(username, password):
+    print "Beginning to fetch the Birthday spreadsheet..."
+    docid = '0AnDRnFzjhfTAdERvbC1YUnJTWlQtR3MwZzhlRERTRUE'
+    gc = gspread.login(username, password);
+    wks = gc.open('Team Birthdays').sheet1
+    spreadsheet = gc.open_by_key(docid)
+    for i, worksheet in enumerate(spreadsheet.worksheets()):
+        filename = 'Birth.csv'
+        with open(filename, 'wb') as f:
+            writer = csv.writer(f)
+            newValues = []
+            for l in worksheet.get_all_values():
+                newSubl = []
+                for s in l:
+                    s = s.replace(',', ';')
+                    newSubl.append(s)
+                newValues.append(newSubl);
+            writer.writerows(newValues)
+    print "Spreadsheet fetching completed."
 
 if __name__ == '__main__':
-    from time import strftime
+
+    if len(sys.argv) != 2:
+        print "Usage: ./birthday username"
+        sys.exit()
+    username = sys.argv[1]
+    password = getpass.getpass()
+    fetchSpreadsheet(username, password)
     day     = strftime("%d")
     month   = strftime("%m")
-    months  = getBirthdays(day, month)
-    months  = getBirthdays(13, 12)
+    getBirthdays(day, month)
+    getBirthdays(13, 12)
+    #os.remove('Birth.csv');
